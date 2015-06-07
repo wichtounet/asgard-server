44b1f99ba1552905cb7f92b8da4dae61b8be03a3
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-06-07 20:00:47 +0200

    Drop root as soon as possible

--- src/server.cpp
+++ src/server.cpp
@@ -47,15 +47,43 @@ void terminate(int /*signo*/){
     abort();
 }
 
+bool revoke_root(){
+    if (getuid() == 0) {
+        if (setgid(1000) != 0){
+            std::cout << "asgard: setgid: Unable to drop group privileges: " << strerror(errno) << std::endl;
+            return false;
+        }
+
+        if (setuid(1000) != 0){
+            std::cout << "asgard: setgid: Unable to drop user privileges: " << strerror(errno) << std::endl;
+            return false;
+        }
+    }
+
+    if (setuid(0) != -1){
+        std::cout << "asgard: manager to regain root privileges, exiting..." << std::endl;
+        return false;
+    }
+
+    return true;
+}
 
 } //end of anonymous namespace
 
 int main(){
+    //Run the wiringPi setup (as root)
+    wiringPiSetup();
+
+    //Drop root privileges and run as pi:pi again
+    if(!revoke_root()){
+       std::cout << "asgard: unable to revoke root privileges, exiting..." << std::endl;
+       return 1;
+    }
+
     //Register signals for "proper" shutdown
     signal(SIGTERM, terminate);
     signal(SIGINT, terminate);
 
-    wiringPiSetup();
     pinMode(1, OUTPUT);
     digitalWrite(1, HIGH);
 
