33dcb3783e113145bfddee9ed2c8c17e821f051b
stephane-ly <stephane.ly@edu.hefr.ch>
2016-06-06 10:03:32 +0200

    Create rules tables

--- src/db.cpp
+++ src/db.cpp
@@ -60,10 +60,10 @@ void create_tables(CppSQLite3DB& db) {
         "create table if not exists sensor(pk_sensor integer primary key autoincrement, type char(20),"
         "name char(20), fk_source integer, foreign key(fk_source) references source(pk_source));");
     db.execDML(
-        "create table if not exists action(pk_action integer primary key autoincrement, type char(20),"
+        "create table if not exists actuator(pk_actuator integer primary key autoincrement,"
         "name char(20), fk_source integer, foreign key(fk_source) references source(pk_source));");
     db.execDML(
-        "create table if not exists actuator(pk_actuator integer primary key autoincrement,"
+        "create table if not exists action(pk_action integer primary key autoincrement, type char(20),"
         "name char(20), fk_source integer, foreign key(fk_source) references source(pk_source));");
     db.execDML(
         "create table if not exists sensor_data(pk_sensor_data integer primary key autoincrement, data char(20),"
@@ -71,6 +71,13 @@ void create_tables(CppSQLite3DB& db) {
     db.execDML(
         "create table if not exists actuator_data(pk_actuator_data integer primary key autoincrement, data char(20),"
         "time datetime not null default current_timestamp, fk_actuator integer, foreign key(fk_actuator) references actuator(pk_actuator));");
+    db.execDML(
+        "create table if not exists condition(pk_condition integer primary key autoincrement," 
+        "value char(20), operator char(20), fk_sensor integer, fk_actuator integer,"
+        "foreign key(fk_sensor) references sensor(pk_sensor), foreign key(fk_actuator) references actuator(pk_actuator));");
+    db.execDML(
+        "create table if not exists rule(pk_rule integer primary key autoincrement, value char(20), fk_condition integer, fk_action integer,"
+        "foreign key(fk_condition) references condition(pk_condition), foreign key(fk_action) references action(pk_action));");
 }
 
 bool db_connect(CppSQLite3DB& db) {
